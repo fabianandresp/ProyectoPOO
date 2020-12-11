818b586af4449e4271b3942d3445a467501b81fe
pimie <pimie@192.168.0.26>
2020-12-10 21:08:05 -0600

    problema 1

---
+++ src/problema1/Convertidora.java
@@ -0,0 +1,76 @@
+package problema1;
+
+import java.util.Scanner;
+
+public class Convertidora {
+
+ Moneda monedaColon;
+    Moneda monedaDolar;
+    Moneda monedaEuro;
+            
+    
+    public Convertidora(){
+    }
+
+    public Convertidora(Moneda monedaColon, Moneda monedaDolar, Moneda monedaEuro) {
+        this.monedaColon = monedaColon;
+        this.monedaDolar = monedaDolar;
+        this.monedaEuro = monedaEuro;
+    }
+    
+    
+    //Este metodo requiere que monedaOrigen y monedaSalida esten establecidas
+    public float convertir(Moneda monedaOrigen, Moneda monedaSalida, float cantidadOrigen){
+        float resultado = 0;
+        if(monedaOrigen!=null && monedaSalida!=null){
+        
+           if(monedaOrigen.getNombre().equals("Colon")){
+               if(monedaSalida.getNombre().equals("Colon")){
+                   resultado = cantidadOrigen;
+               }
+               else if(monedaSalida.getNombre().equals("Dolar")){
+                   resultado = cantidadOrigen/monedaDolar.getValorColon();
+               }
+               else{
+                   resultado = cantidadOrigen/monedaEuro.getValorColon();
+               }
+           }
+           else if(monedaOrigen.getNombre().equals("Dolar")){
+           
+           }
+           else {
+           
+           }
+        }
+        return resultado;
+    }
+
+    public Moneda getMonedaColon() {
+        return monedaColon;
+    }
+
+    public void setMonedaColon(Moneda monedaColon) {
+        this.monedaColon = monedaColon;
+    }
+
+    public Moneda getMonedaDolar() {
+        return monedaDolar;
+    }
+
+    public void setMonedaDolar(Moneda monedaDolar) {
+        this.monedaDolar = monedaDolar;
+    }
+
+    public Moneda getMonedaEuro() {
+        return monedaEuro;
+    }
+
+    public void setMonedaEuro(Moneda monedaEuro) {
+        this.monedaEuro = monedaEuro;
+    }
+    
+    
+ 
+
+    }
+
---
+++ src/problema1/Tester.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pimie
+ */
+public class Tester {
+
+     public static void main(String[] args){
+        //CONFIGURAR MONEDAS
+         Moneda mColon = new Moneda("Colon", 1);
+         Moneda mDolar = new Moneda("Dolar", 500);
+         Moneda mEuro = new Moneda("Euro", 765);
+         
+        //INICIA 
+        Scanner s = new Scanner(System.in);
+        Convertidora maq = new Convertidora(mColon,mDolar,mEuro);
+        //maq.setMonedaColon(mColon);
+        //maq.setMonedaDolar(mDolar);
+        //maq.setMonedaEuro(mEuro);
+        
+        while(true){
+            System.out.println("Digite la moneda de origen 1=Colon 2=Dolar 3=Euro");
+            int numMonedaOrigen = s.nextInt();
+            System.out.println("Digite la moneda de salida 1=Colon 2=Dolar 3=Euro");
+            int numMonedaSalida = s.nextInt();
+            System.out.println("Digite la cantidad");
+            int cantidad = s.nextInt();
+
+            if(numMonedaOrigen==1){//Colon
+                if(numMonedaSalida==1){//Colon
+                    System.out.println("El valor equivalente es: "+maq.convertir(mColon, mColon, cantidad));
+                }
+                else if(numMonedaSalida==2){ //Dolar
+                    System.out.println("El valor equivalente es: "+maq.convertir(mColon, mDolar, cantidad)); 
+                }
+                else { //euro
+                    System.out.println("El valor equivalente es: "+maq.convertir(mColon, mEuro, cantidad)); 
+                }
+            }
+            else if(numMonedaOrigen==2){//Dolar
+                if(numMonedaSalida==1){//Colon
+                    System.out.println("El valor equivalente es: "+maq.convertir(mDolar, mColon, cantidad));
+                }
+                else if(numMonedaSalida==2){ //Dolar
+                    System.out.println("El valor equivalente es: "+maq.convertir(mDolar, mDolar, cantidad)); 
+                }
+                else { //euro
+                    System.out.println("El valor equivalente es: "+maq.convertir(mDolar, mEuro, cantidad)); 
+                }
+            }
+            else{ //Euro
+
+               if(numMonedaSalida==1){//Colon
+                    System.out.println("El valor equivalente es: "+maq.convertir(mEuro, mColon, cantidad));
+                }
+                else if(numMonedaSalida==2){ //Dolar
+                    System.out.println("El valor equivalente es: "+maq.convertir(mEuro, mDolar, cantidad)); 
+                }
+                else { //euro
+                    System.out.println("El valor equivalente es: "+maq.convertir(mEuro, mEuro, cantidad)); 
+                } 
+            }
+        }
+    }
+
+}
